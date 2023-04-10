1b52b923238107a93f86ccd98b5a6529e278c077
Renardo Williams <rennwill@gmail.com>
2023-04-10 12:27:36 -0400

    lab complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form")
+  form.addEventListener('submit',(e) => {
+    e.preventDefault()
+    listHandler()
+    form.reset()
+  })
 });
+
+function listHandler(){
+
+  const newTask = document.getElementById("new-task-description");
+  let newLi = document.createElement('li')
+  let button = document.createElement('button')
+  button.addEventListener('click',() => {newLi.remove()})
+  button.textContent = "remove"
+  newLi.textContent = newTask.value
+  newLi.appendChild(button)
+  document.getElementById('tasks').appendChild(newLi)
+
+}
